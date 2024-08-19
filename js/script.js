66c723248748d70cf55ac33aab614d1d6ceb110b
Binary-Scribe <theekshanachamodhya@gmail.com>
2024-08-19 05:01:55 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// script.js
+
+// Load footer.html into the footer-placeholder div
+fetch('../pages/footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footer-placeholder').innerHTML = data;
+    })
+    .catch(error => console.error('Error loading footer:', error));
+
+
+fetch('../pages/header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header-placeholder').innerHTML = data;
+    })
+    .catch(error => console.error('Error loading footer:', error));
